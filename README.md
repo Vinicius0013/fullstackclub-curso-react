44a9c1b7af44b418802842e0ad52079a86dabb86
Vinicius Benedito <viniciusaugustobenedito@gmail.com>
2026-03-14 19:02:49 -0300

    Finalização do projeto de gerenciador de tarefas, contendo criação de componentes para reutilização de código, além da estrutura de navegação entre páginas com a utilização da biblioteca 'React Router'.

--- README.md
+++ README.md
@@ -29,6 +29,16 @@ Colocar dentro do atributo `content` os seguintes valores:
 Caso de dúvidas acessar o link abaixo e seguir o passo a passo de configuração para projetos `React + Vite`:
 https://v3.tailwindcss.com/docs/guides/vite
 
+
+# React Router
+Biblioteca `React Router` que gerência rotas na aplicação React
+
+Comando para instalar a versão utilizada:
+`npm install react-router-dom@6.26.1`
+
+Em caso de dúvidas consultar a documentação pelo link abaixo:
+https://reactrouter.com/6.30.3/start/tutorial
+
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
@@ -37,3 +47,17 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+# JSONPlaceholder
+
+Utilização da API gratuita JSONPlacelhoder para realizações de testes de integra na aplicação react.
+Exemplo de requisição:
+`fetch('https://jsonplaceholder.typicode.com/todos/1').then(response => response.json()).then(json => console.log(json))`
+
+Resultado:
+{
+  "userId": 1,
+  "id": 1,
+  "title": "delectus aut autem",
+  "completed": false
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AddTask from './components/AddTask';
 import Tasks from './components/Tasks';
+import { v4 } from 'uuid';
+import Title from './components/Title';
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      title: "Estudar programação",
-      description: "Estudar programação para aprimorar conhecimentos.",
-      isCompleted: false,
-    },
-    {
-      id: 1,
-      title: "Estudar inglês",
-      description: "Estudar inglês para aprimorar conhecimentos.",
-      isCompleted: false,
-    },
-    {
-      id: 3,
-      title: "Estudar espanhol",
-      description: "Estudar espanhol para aprimorar conhecimentos.",
-      isCompleted: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  }, [tasks]);
+
+  // SE QUISER, PODE REATIIVAR O MÉTODO ABAIXO PARA REALIZAR A CHAMADA NA API
+  // E RECUPERAR AS TAREFAS.
+  // useEffect(() => {
+  //   const fetchTasks = async () => {
+  //     const response = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=10', {
+  //       method: 'GET'
+  //     });
+
+  //     const data = await response.json();
+  //     setTasks(data);
+  //   }
+
+  //   fetchTasks();
+  // }, []);
 
   function onTaskClick(taskId) {
     const newTasks = tasks.map(task => {
@@ -37,12 +41,33 @@ function App() {
     setTasks(newTasks);
   }
 
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id != taskId);
+    setTasks(newTasks);
+  }
+
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: v4(),
+      title,
+      description,
+      isCompleted: false
+    };
+    setTasks([...tasks, newTask]);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
-        <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
-        <AddTask />
-        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+      <div className="w-[500px] space-y-4">
+        <Title>Gerenciador de Tarefas</Title>
+        <AddTask
+          onAddTaskSubmit={onAddTaskSubmit}
+        />
+        <Tasks 
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
       </div>
     </div>
   );
